018f8989cc6fb905e36a0555e8e3ae538ea4ad65
Frank Herrera <lscfrancisco@hotmail.com>
2020-10-15 16:19:52 -0500

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB;
 if( process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/appmeco';
 } else {
-    urlDB = 'mongodb+srv://lscfrank:mYxg6IYAQFVMpv9P@cluster0.cnyxa.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
